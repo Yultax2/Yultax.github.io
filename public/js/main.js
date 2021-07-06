630676c5f860074c212f284a8c266f3e21575c7f
Tuhana GAYRETLİ <iamtuhana@gmail.com>
2021-07-06 12:12:19 +0300

    added things i love

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,71 @@
 const otherElements = {
   "i-know": document.querySelector(".i-know > .badges"),
-  "i-love": document.querySelector(".i-know > .badges"),
+  "i-love": document.querySelector(".i-love > .badges"),
 };
 
-const languages = [
-  {
-    name: "JavaScript",
-    color: "#F7DF1E",
-    textColor: "black",
-  },
-  {
-    name: "TypeScript",
-    color: "#3178C6",
-  },
-  {
-    name: "Python",
-    color: "#3776AB",
-  },
-  {
-    name: "HTML",
-    color: "#E34F26",
-  },
-  {
-    name: "Sass",
-    color: "#CC6699",
-  },
-  {
-    name: "Bash",
-    color: "#4EAA25",
-  },
-];
+const badges = {
+  languages: [
+    {
+      name: "JavaScript",
+      color: "#F7DF1E",
+      textColor: "black",
+    },
+    {
+      name: "TypeScript",
+      color: "#3178C6",
+    },
+    {
+      name: "Python",
+      color: "#3776AB",
+    },
+    {
+      name: "HTML",
+      color: "#E34F26",
+    },
+    {
+      name: "Sass",
+      color: "#CC6699",
+    },
+    {
+      name: "Bash Script",
+      filename: "bash",
+      color: "#4EAA25",
+    },
+  ],
+  others: [
+    {
+      name: "Linux",
+      color: "#FCC624",
+    },
+    {
+      name: "GNU/Linux",
+      filename: "gnu",
+      color: "#A42E2B",
+    },
+    {
+      name: "Arch Linux",
+      filename: "archlinux",
+      color: "#1793D1",
+    },
+    {
+      name: "Android",
+      color: "#3DDC84",
+    },
+    {
+      name: "Firefox",
+      color: "#FF7139",
+    },
+    {
+      name: "Osu!",
+      filename: "osu",
+      color: "#FF66AA",
+    },
+  ],
+};
 
-for (let i = 0; i < languages.length; i++) {
-  const el = languages[i],
+// I Know
+for (let i = 0; i < badges.languages.length; i++) {
+  const el = badges.languages[i],
     div = document.createElement("div"),
     p = document.createElement("p"),
     img = document.createElement("img");
@@ -40,7 +73,7 @@ for (let i = 0; i < languages.length; i++) {
   p.innerHTML = el.name;
   p.style.color = el?.textColor;
 
-  img.src = `./public/img/svg/${el.name.toLowerCase()}.svg`;
+  img.src = `./public/img/svg/${el?.filename || el.name.toLowerCase()}.svg`;
 
   div.className = el.name.toLowerCase();
   div.style.backgroundColor = el.color;
@@ -50,4 +83,26 @@ for (let i = 0; i < languages.length; i++) {
   div.appendChild(p);
 
   otherElements["i-know"].appendChild(div);
+}
+
+// I Love
+for (let i = 0; i < badges.others.length; i++) {
+  const el = badges.others[i],
+    div = document.createElement("div"),
+    p = document.createElement("p"),
+    img = document.createElement("img");
+
+  p.innerHTML = el.name;
+  p.style.color = el?.textColor;
+
+  img.src = `./public/img/svg/${el?.filename || el.name.toLowerCase()}.svg`;
+
+  div.className = el.name.toLowerCase();
+  div.style.backgroundColor = el.color;
+  div.style.color = el?.textColor;
+
+  div.appendChild(img);
+  div.appendChild(p);
+
+  otherElements["i-love"].appendChild(div);
 }
