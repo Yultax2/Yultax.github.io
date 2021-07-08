8f49a1259424f11ed8511dce6bcf957169de90b1
Tuhana GAYRETLİ <iamtuhana@gmail.com>
2021-07-09 02:04:16 +0300

    imgs & index.html fixes

---
+++ config.ts
@@ -0,0 +1,88 @@
+export default {
+  id: "852582281112715284",
+  me: {
+    header: "Welcome to my website! I'm Tuhana.",
+    description:
+      "I'm 17 years old developer from Turkey. I love cats sooooooo much 🐱🥰",
+  },
+  badges: {
+    languages: [
+      {
+        name: "JavaScript",
+      },
+      {
+        name: "TypeScript",
+      },
+      {
+        name: "Deno",
+      },
+      {
+        name: "Python",
+      },
+      {
+        name: "HTML",
+      },
+      {
+        name: "Sass",
+      },
+      {
+        name: "Bash Script",
+        fileName: "bash",
+      },
+    ],
+    others: [
+      {
+        name: "Linux",
+      },
+      {
+        name: "GNU/Linux",
+        fileName: "gnu",
+      },
+      {
+        name: "Arch Linux",
+        fileName: "archlinux",
+      },
+      {
+        name: "GNOME",
+      },
+      {
+        name: "Git",
+      },
+      {
+        name: "Android",
+      },
+      {
+        name: "Vim",
+      },
+      {
+        name: "Visual Studio Code",
+        fileName: "visualstudiocode",
+      },
+      {
+        name: "Firefox",
+      },
+      {
+        name: "Osu!",
+        fileName: "osu",
+      },
+    ],
+  },
+  footer: [
+    {
+      name: "Discord",
+      url: "https://discord.com/users/852582281112715284",
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com/iamtuhana",
+    },
+    {
+      name: "Spotify",
+      url: "https://open.spotify.com/user/4aiw41dacy2h3arnqb6jug6iu",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/iamtuhana",
+    },
+  ],
+};
